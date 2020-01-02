a36561564dc65941aaa19cb0fd8f0a1837704c00
lyn <lyn@hakdokman.com>
2020-01-02 16:53:05 +0900

    start_crawling.sh>주석추가

--- start_crawling.sh
+++ start_crawling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source ~/reporter_hdm/botvenv/bin/activate
-curl https://hakdokman.slack.com/api/auth.test\?token\=xoxb-325351176550-888847745232-ahkVBaVUVO2kyKSCBRL25kPU
-export SLACK_BOT_TOKEN="xoxb-325351176550-888847745232-ahkVBaVUVO2kyKSCBRL25kPU"
-export BOT_ID='US4QXMX6U'
-python3 ~/reporter_hdm/news_crawling.py >> ~/reporter_hdm/crontab_log2.txt 2>&1
+source ~/reporter_hdm/botvenv/bin/activate    #botvenv 가상환경실행
+curl https://hakdokman.slack.com/api/auth.test\?token\=xoxb-325351176550-888847745232-ahkVBaVUVO2kyKSCBRL25kPU    #api 서버와 통신
+export SLACK_BOT_TOKEN="xoxb-325351176550-888847745232-ahkVBaVUVO2kyKSCBRL25kPU"  # 토큰
+export BOT_ID='US4QXMX6U'   #BOT ID
+python3 ~/reporter_hdm/news_crawling.py   #python 크롤링 파일 실행
