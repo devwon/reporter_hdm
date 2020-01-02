543bcd1aee26d7e95ce3e3c447381485861480be
Kim JeongHyeon <tibber@hakdokman.com>
2020-01-02 18:02:52 +0900

    curl 시 토큰 중복 수정 및 스크립트 실행 경로 변경

--- start_crawling.sh
+++ start_crawling.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-source ~/reporter_hdm/botvenv/bin/activate    #botvenv 가상환경실행
-curl https://hakdokman.slack.com/api/auth.test\?token\=xoxb-325351176550-888847745232-ahkVBaVUVO2kyKSCBRL25kPU    #api 서버와 통신
+source venv/bin/activate    #botvenv 가상환경실행
 export SLACK_BOT_TOKEN="xoxb-325351176550-888847745232-ahkVBaVUVO2kyKSCBRL25kPU"  # 토큰
 export BOT_ID='US4QXMX6U'   #BOT ID
-python3 ~/reporter_hdm/news_crawling.py   #python 크롤링 파일 실행
+curl https://hakdokman.slack.com/api/auth.test\?token\=\{$SLACK_BOT_TOKEN\}    #api 서버와 통신
+python3 news_crawling.py   #python 크롤링 파일 실행
+
