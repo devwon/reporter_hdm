80a8d917ac05ca8e374cee0d4536fb50502713cf
lyn <lyn@hakdokman.com>
2020-01-09 16:57:17 +0900

    도커화 작업>path 지정

--- start_crawling.sh
+++ start_crawling.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-source ~/reporter_hdm/venv/bin/activate    #botvenv 가상환경실행
+
+#path 지정
+export DIR_NAME=`dirname "$0"`
+export DIR_HOME=`cd $DIR_NAME; cd ..; pwd`
+
+echo 'dir home: '$DIR_HOME '\ndir name: '$DIR_NAME
+
+#source $DIR_HOME/reporter_hdm/botvenv/bin/activate    #botvenv 가상환경실행
 export SLACK_BOT_TOKEN="xoxb-325351176550-888847745232-ahkVBaVUVO2kyKSCBRL25kPU"  # 토큰
 export BOT_ID='US4QXMX6U'   #BOT ID
 curl -sS https://hakdokman.slack.com/api/auth.test\?token\=\{$SLACK_BOT_TOKEN\}    #api 서버와 통신
-python3 ~/reporter_hdm/news_crawling.py   #python 크롤링 파일 실행
-
+python3 $DIR_HOME/reporter_hdm/news_crawling.py   #python 크롤링 파일 실행
