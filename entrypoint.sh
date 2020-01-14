83403fe01964c1ad8be221c3f511858c1b2d6cc3
lyn <lyn@hakdokman.com>
2020-01-14 14:45:41 +0900

    주석 추가

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # cron 시작
 cron
-# container 시작 문구
+# container 시작시 문구 STDOUT으로 노출(docker logs로 노출)
 echo container is started!!: `date` > /proc/1/fd/1 2>/proc/1/fd/2
 # container 상태 계속 유지
 while : ; do
